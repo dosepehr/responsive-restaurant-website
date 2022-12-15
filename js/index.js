29baf343611deef9e4a9514dd119de5930f8ae57
sepehr <dosepehr@gmail.com>
2022-12-15 04:06:01 +0400

    header animation is done

--- js/index.js
+++ js/index.js
@@ -3,15 +3,23 @@ const dark_mode = document.querySelector('.dark-mode-toggler');
 const moon = document.querySelector('.bi-moon');
 const sun = document.querySelector('.bi-sun');
 
-
-
-
-
-console.log(dark_mode);
-
-
 dark_mode.addEventListener('click', () => {
     body.classList.toggle('dark');
     moon.classList.toggle('hidden');
     sun.classList.toggle('hidden');
+})
+
+
+
+const header = document.querySelector('header');
+
+
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-down', window.scrollY >= 100);
+})
+
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-up', window.scrollY < 100);
 })
