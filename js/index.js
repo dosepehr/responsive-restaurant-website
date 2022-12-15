00581175654f72ba3436e1474e4b8e2e7ab6e48d
sepehr <dosepehr@gmail.com>
2022-12-15 14:55:22 +0400

    sidebar animation done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+/** selectors */
 const body = document.querySelector('body');
 const dark_mode = document.querySelector('.dark-mode-toggler');
 const moon = document.querySelector('.bi-moon');
 const sun = document.querySelector('.bi-sun');
+
+const header = document.querySelector('header');
+const open_sidebar = document.querySelector('.bi-list');
+const sidebar = document.querySelector('.side-bar');
+const close_sidebar = document.querySelector('.bi-x');
+const overlay = document.querySelector('.overlay');
+
+
+
+
+
 
 dark_mode.addEventListener('click', () => {
     body.classList.toggle('dark');
@@ -11,7 +23,6 @@ dark_mode.addEventListener('click', () => {
 
 
 
-const header = document.querySelector('header');
 
 
 
@@ -23,3 +34,27 @@ window.addEventListener('scroll', () => {
 window.addEventListener('scroll', () => {
     header.classList.toggle('header-up', window.scrollY < 100);
 })
+
+
+
+
+open_sidebar.addEventListener('click', () => {
+    toggle_sidebar();
+})
+close_sidebar.addEventListener('click', () => {
+    toggle_sidebar();
+
+})
+overlay.addEventListener('click', () => {
+    toggle_sidebar();
+})
+
+
+
+
+
+
+const toggle_sidebar = () => {
+    sidebar.classList.toggle('side-bar-active');
+    overlay.classList.toggle('overlay-active');
+}
